4b9f2b08739d4d865b5578083e182f94d97385ef
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2021-01-28 23:43:30 -0300

    feat: aplying component Layout wrapping all content

--- pages/posts/first-attempt.js
+++ pages/posts/first-attempt.js
@@ -1,18 +1,52 @@
+import Head from "next/head";
 import Link from "next/link";
 
 import styles from "../../styles/Home.module.css";
 
+import Layout from "../../components/Layout";
+
 export default function firstAttempt() {
   return (
-    <div className={styles.container}>
-      <h1 className={styles.title}>
-        This is my first attempt with NextJS{" "}
-        <Link href="/">
-          <h4>
-            <a>Back</a>
-          </h4>
-        </Link>
-      </h1>
-    </div>
+    <Layout>
+      <div className={styles.container}>
+        <Head>
+          <title>{firstAttempt.name}</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+        <h1 className={styles.title}>
+          This is my first attempt with NextJS{" "}
+          <Link href="/">
+            <h4>
+              <a>Back</a>
+            </h4>
+          </Link>
+        </h1>
+
+        <p className={styles.description}>
+          First of all, it was clear how easy is to create a new page: fast as
+          creating another js file. The route is auto-generated and it respects
+          the path of the file. To exemplify what was just said, if you have a
+          file placed in a NextJS project as{" "}
+          <code className={styles.code}>pages/posts/first-attempt.js</code> and
+          run it, you'll find a page already available at
+          localhost:3000/posts/first-attempt. Just like that.
+        </p>
+
+        <p className={styles.description}>
+          A bunch of useful libs are loaded with NextJS and one of the
+          highlights are that ones used for Styling. By the way, style is a
+          great deal here too. NextJS comes with Sass and a minor CSS-in-JS lib
+          called <code className={styles.code}>styled-jsx</code> that does what
+          is called means: applying css directly in JSX comeponents
+          (documentation can be found{" "}
+          <Link href="https://github.com/vercel/styled-jsx">here</Link>). Two
+          approuches are recommended by the tutorial: use of CSS Modules to
+          handle component styling (with optimization for lazy loading and so
+          on) and importing global CSS file directly in{" "}
+          <code className={styles.code}>/pages/_app.js</code>.
+        </p>
+      </div>
+    </Layout>
   );
 }
